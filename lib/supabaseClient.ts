726326fc95e093d1b30cb984e8a3ab98f52f0606
장준서(Jang <joonseojang@gsenc.com>
2025-11-28 14:36:49 +0900

    Fix supabase client and update dependencies

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
+// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  'https://yuawxjypxfkwfcmalhbg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1YXd4anlweGZrd2ZjbWFsaGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDY2NzAsImV4cCI6MjA3OTY4MjY3MH0.85j7eV-8o4JIPSPxjkGdMNAA2ff-f5x4CpI9SzZkLxM',
 );
